3edbf104532485364d89623b25f6115097b47780
vin96 <vin96@users.noreply.github.com>
2017-05-02 10:25:48 -0400

    Indoor Positioning System Updated Documentation + Code

---
+++ 5_Source_Code/Indoor Positioning System [FINAL UPDATED]/Indoor Positioning System Code/Android App for IPS Demo/src/main/java/comrusegroup12se/github/indoorpositioningsystem/MainActivity.java
@@ -0,0 +1,43 @@
+package comrusegroup12se.github.indoorpositioningsystem;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // Example of a call to a native method
+    TextView tv = (TextView) findViewById(R.id.textView);
+
+        tv.setText("Indoor Tracking is Enabled");
+
+
+
+
+
+
+           while(true){
+
+               stringFromJNI();
+
+           }
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
